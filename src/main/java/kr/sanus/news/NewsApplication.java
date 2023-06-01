b4512b09c9d6221b16f2c17188820ab843d62afd
Shokoku <shokoku@outlook.kr>
2023-06-02 02:06:46 +0900

    [add] Member Join

--- src/main/java/kr/sanus/news/NewsApplication.java
+++ src/main/java/kr/sanus/news/NewsApplication.java
@@ -2,7 +2,9 @@ package kr.sanus.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class NewsApplication {
 
---
+++ src/main/java/kr/sanus/news/member/MemberEntity.java
@@ -0,0 +1,48 @@
+package kr.sanus.news.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import kr.sanus.news.base.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "member")
+public class MemberEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String mobile;
+
+    public MemberDto toDto() {
+        return MemberDto.builder()
+            .id(id)
+            .email(email)
+            .name(name)
+            .mobile(mobile)
+            .build();
+    }
+}
