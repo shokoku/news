81e8d7a0efd5ad8fcc1b41ce9d5ae6b152767725
Shokoku <shokoku@outlook.kr>
2023-06-01 18:12:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# News Project
+
+## 개발환경
+* SpringBoot: 3.1.0
+* JDK: corretto 17.0.7
+* Database: My-SQL 8.0.32
+
+## Dependency
+* Spring Web
+* Spring Data Jpa
+* Spring Security
+* Thymeleaf
+* Lombok
---
+++ src/test/java/kr/sanus/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.sanus.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
